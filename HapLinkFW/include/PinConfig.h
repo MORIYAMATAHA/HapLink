435d4535121874efd3ac72acd8e0b56c3ab5cee3
森山多霸 <taha.moriyama@gmail.com>
2021-06-13 15:28:01 +0900

    Added PinConfig

---
+++ HapLinkFW/include/PinConfig.h
@@ -0,0 +1,29 @@
+/*----------encoder_pin設定------------*/
+//左側のベース→右モータ
+#define L_R_enc_A 36
+#define L_R_enc_B 39
+//左側のベース→左モータ
+#define L_L_enc_A 35
+#define L_L_enc_B 34
+//右側のベース→右モータ
+#define R_R_enc_A 22
+#define R_R_enc_B 23
+//右側のベース→左モータ
+#define R_L_enc_A 16
+#define R_L_enc_B 17
+
+
+/*--------モータドライバpin設定-------------*/
+//左側のベース→右モータ
+#define L_R_FIN 4
+#define L_R_RIN 25
+//左側のベース→左モータ
+#define L_L_FIN 15
+#define L_L_RIN 2
+
+//右側のベース→右モータ
+#define R_R_FIN 12
+#define R_R_RIN 13
+//右側のベース→左モータ
+#define R_L_FIN 26
+#define R_L_RIN 27
